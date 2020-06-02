cc21a816c864b2d05d1499f921033e320450d9e3
Anthony Johnson <58479386+anthonyj713@users.noreply.github.com>
2020-06-02 16:38:18 -0600

    MVP met

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,14 +12,14 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print("X is %i, y is %f, z is %s" %(10, 2.24552, "I like turtles!"))
+print("X is %i, y is %.2f, z is %s" %(10, 2.24552, "I like turtles!"))
 
 # Use the 'format' string method to print the same thing
 
-print('x is {}, y is {}, z is {}!'.format('10','2.24552','I like turtles'))
+print('x is {}, y is {}, z is {}!'.format('10','2.25','I like turtles'))
 
 # Finally, print the same thing using an f-string
 x = 10
-y = 2.24552
+y = 2.25
 z = "I like turtles!"
 print(F"x is {x}, y is {y}, z is {z}")
